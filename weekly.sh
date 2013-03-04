d0b4d5a6c9f3574a5e9b366b262b524bc8b4c380
Matthias Bauer <moeffju@moeffju.net>
2013-03-04 11:24:08 +0100

    Add --trust-me flag to weekly runner

--- weekly.sh
+++ weekly.sh
@@ -2,8 +2,10 @@
 
 monday=$(python -c "import dateutil.parser; from dateutil.relativedelta import relativedelta; x = dateutil.parser.parse('monday'); y = x + relativedelta(weeks=-1); print y.strftime('%Y-%m-%d')")
 
-echo "Running update for $monday, ENTER to confirm"
-read
+if [ "x$1" != "x--trust-me" ]; then
+  echo "Running update for $monday, ENTER to confirm"
+  read
+fi
 
 ./weekly-update.py $monday
 
