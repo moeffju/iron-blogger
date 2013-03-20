52d5b77c1638edf77691c54da7b2cc2d08aabd1c
Matthias Bauer <moeffju@moeffju.net>
2013-03-20 12:45:18 +0100

    Smooth

--- weekly.sh
+++ weekly.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 monday=$(python -c "import dateutil.parser; from dateutil.relativedelta import relativedelta; x = dateutil.parser.parse('monday'); y = x + relativedelta(weeks=-1); print y.strftime('%Y-%m-%d')")
 
 if [ "x$1" != "x--trust-me" ]; then
   echo "Running update for $monday, ENTER to confirm"
-  read
+  read nothing
 fi
 
 ./weekly-update.py $monday
